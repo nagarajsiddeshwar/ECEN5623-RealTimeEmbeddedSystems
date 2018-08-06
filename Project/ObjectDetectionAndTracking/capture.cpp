3d60739d40399682148cab56dc53d840e94afb1a
nagarajsiddeshwar <nagarajcs05@gmail.com>
2018-08-05 19:30:10 -0600

    Added centroid instantenous tracing

---
+++ Project/ObjectDetectionAndTracking/capture.cpp
@@ -0,0 +1,156 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include<pthread.h>
+#include <X11/Xlib.h>
+#include <sched.h>
+#include <time.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv/highgui.h>
+#include "opencv2/objdetect/objdetect.hpp"
+
+#include <semaphore.h>
+
+
+#include <string.h>
+
+#include <syslog.h>
+#include <math.h>
+#include <sys/param.h>
+#include <sys/time.h>
+#include <errno.h>
+
+using namespace cv;
+using namespace std;
+
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+    int ilastx=-1,ilasty=-1; 
+    //int iLastY = -1;
+
+RNG rng(12345);
+
+ int main( int argc, char** argv )
+ {
+    VideoCapture cap(0); //capture the video from web cam
+
+    if ( !cap.isOpened() )  // if not success, exit program
+    {
+         cout << "Cannot open the web cam" << endl;
+         return -1;
+    }
+
+    namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+
+ int iLowH = 0;
+ int iHighH = 179;
+
+ int iLowS = 0; 
+ int iHighS = 255;
+
+ int iLowV = 0;
+ int iHighV = 255;
+
+
+
+ //Create trackbars in "Control" window
+ cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+ cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+
+ cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+ cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+
+ cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+ cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+
+    while (true)
+    {
+        Mat imgOriginal;
+
+        bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+
+         if (!bSuccess) //if not success, break loop
+        {
+             cout << "Cannot read a frame from video stream" << endl;
+             break;
+        }
+
+  Mat imgHSV;
+
+  cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+  blur( imgHSV,imgHSV, Size(3,3) );
+ 
+  Mat imgThresholded;
+
+  inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+      
+  //morphological opening (remove small objects from the foreground)
+  erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+  dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+
+  //morphological closing (fill small holes in the foreground)
+  dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+  erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+  findContours( imgThresholded, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+  vector<vector<Point> > contours_poly( contours.size() );
+ vector<Rect> boundRect( contours.size() );
+  vector<Point2f>center( contours.size() );
+  vector<float>radius( contours.size() );
+
+
+  for( int i = 0; i < contours.size(); i++ )
+     { 
+       approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+      boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+   //    minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+     }
+
+  Mat drawing = Mat::zeros(  imgThresholded.size(), CV_8UC3 );
+  for( int i = 0; i< contours.size(); i++ )
+     {
+       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+ 
+    //  drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+   
+   rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+   Point center_of_rect = (boundRect[i].br() + boundRect[i].tl())*0.5;
+    circle(drawing ,center_of_rect,3,Scalar(255,255,255));
+
+ // iLast.x = boundRect[i].br();
+  //             iLast.y = boundRect[i].tl();
+
+printf("\n %d %d \n",boundRect[i].br().x*0.5+boundRect[i].tl().x*0.5,boundRect[i].br().y*0.5+boundRect[i].tl().y*0.5);
+
+            if (ilastx >= 0 && ilasty >= 0 && (boundRect[i].br().y*0.5+boundRect[i].tl().y*0.5) >= 0 && (boundRect[i].br().x*0.5+boundRect[i].tl().x*0.5) >= 0)
+               {
+                //Draw a red line from the previous point to the current point
+                line(drawing, Point(boundRect[i].br().x*0.5+boundRect[i].tl().x*0.5, boundRect[i].br().y*0.5+boundRect[i].tl().y*0.5), Point(ilastx, ilasty), Scalar(0,0,255), 2);
+               }
+  ilastx= boundRect[i].br().x*0.5+boundRect[i].tl().x*0.5;
+   ilasty= boundRect[i].br().y*0.5+boundRect[i].tl().y*0.5;
+
+  }
+
+  
+  imshow("Thresholded Image", imgThresholded); //show the thresholded image
+  imshow("Original", imgOriginal); //show the original image
+  imshow( "Contours",drawing );
+
+ imshow( "Contours", drawing );
+
+        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+       {
+            cout << "esc key is pressed by user" << endl;
+            break; 
+       }
+    }
+
+   return 0;
+
+}
